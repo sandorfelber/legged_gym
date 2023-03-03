e42afbac31e77685af86874dbbad11912a5c3148
Bretagne Antonin <antonin.bretagne@ens-paris-saclay.fr>
2023-03-03 18:43:31 +0000

    fixed visual problem

--- legged_gym/envs/solo12/solo12_config.py
+++ legged_gym/envs/solo12/solo12_config.py
@@ -1,3 +1,95 @@
+#from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+#
+#class Solo12Cfg( LeggedRobotCfg ):
+#        
+#    class env( LeggedRobotCfg.env):
+#        num_envs = 1
+#
+#    class init_state( LeggedRobotCfg.init_state ):
+#
+#    ### Initial state parameters ###
+#        pos = [0.0, 0.0, 0.23]  # x,y,z [m]
+#        default_joint_angles = {  # = target angles [rad] when action = 0.0
+#            "FL_hip_joint": 0.01,
+#            "RL_hip_joint": 0.01,
+#            "FR_hip_joint": -0.01,
+#            "RR_hip_joint": -0.01,
+#            "FL_thigh_joint": 0.8,
+#            "RL_thigh_joint": 0.8,
+#            "FR_thigh_joint": 0.8,
+#            "RR_thigh_joint": 0.8,
+#            "FL_calf_joint": -1.6,
+#            "RL_calf_joint": -1.6,
+#            "FR_calf_joint": -1.6,
+#            "RR_calf_joint": -1.6,
+#        }
+#
+#    class control( LeggedRobotCfg.control ):
+#        control_type = "P"
+#        stiffness = {"joint": 3.0}  # [N*m/rad]
+#        damping = {"joint": 0.3}  # [N*m*s/rad]
+#        # action scale: target angle = actionScale * action + defaultAngle
+#        action_scale = 0.25
+#        hip_scale_reduction = 1.0
+#        # decimation: Number of control action updates @ sim DT per policy DT
+#        decimation = 4
+#
+#
+#    class asset( LeggedRobotCfg.asset ):
+#            file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/solo12/solo12_isaac.urdf'
+#            foot_name = "None"  # name of the feet bodies, used to index body state and contact force tensors
+#            penalize_contacts_on = []
+#            terminate_after_contacts_on = []
+#            disable_gravity = False
+#            # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
+#            collapse_fixed_joints = True
+#            fix_base_link = False  # fixe the base of the robot
+#            default_dof_drive_mode = 3  # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
+#            self_collisions = 0  # 1 to disable, 0 to enable...bitwise filter
+#            # replace collision cylinders with capsules, leads to faster/more stable simulation
+#            replace_cylinder_with_capsule = True
+#            flip_visual_attachments = (
+#                  False  # Some .obj meshes must be flipped from y-up to z-up
+#            )
+#
+#            density = 0.001
+#            angular_damping = 0.0
+#            linear_damping = 0.0
+#            max_angular_velocity = 1000.0
+#            max_linear_velocity = 1000.0
+#            armature = 0.0
+#            thickness = 0.01
+#
+#
+#    class sim( LeggedRobotCfg.sim ):
+#            dt = 0.005
+#            substeps = 1
+#            gravity = [0.0, 0.0, -9.81]  # [m/s^2]
+#            up_axis = 1  # 0 is y, 1 is z
+#
+#    #        use_gpu_pipeline = True
+#
+#            class physx( LeggedRobotCfg.sim.physx ):
+#                num_threads = 10
+#                solver_type = 1  # 0: pgs, 1: tgs
+#                num_position_iterations = 4
+#                num_velocity_iterations = 0
+#                contact_offset = 0.01  # [m]
+#                rest_offset = 0.0  # [m]
+#                bounce_threshold_velocity = 0.5  # 0.5 [m/s]
+#                max_depenetration_velocity = 1.0
+#                max_gpu_contact_pairs = 2**23  # 2**24 -> needed for 8000 envs and more
+#                default_buffer_size_multiplier = 5
+#                contact_collection = (
+#                    2  # 0: never, 1: last sub-step, 2: all sub-steps (default=2)
+#                )
+#
+#class Solo12CfgPPO( LeggedRobotCfgPPO ):
+#    
+#    class runner( LeggedRobotCfgPPO.runner ):
+#        run_name = ''
+#        experiment_name = 'solo12'
+
 
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
@@ -68,14 +160,17 @@ class Solo12Cfg( LeggedRobotCfg ):
             smoothness_2 = -1.5 # -c_a2
             
     class commands( LeggedRobotCfg.commands ):
+        pass
     #    num_commands = 3 # lin_vel_x, lin_vel_y, ang_vel_yaw
-        heading_command = False
+    #    heading_command = False
 
     class asset( LeggedRobotCfg.asset ):
         file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/solo12/solo12_isaac.urdf'
         name = "solo"
         foot_name = 'calf'
         collapse_fixed_joints = False # otherwise feet are collapsed, and robot.feet_indices is not populated
+      #  default_dof_drive_mode = 1 # pos tgt
+        flip_visual_attachments = False # fix visual problem with meshes
 
 class Solo12CfgPPO( LeggedRobotCfgPPO ):
     
