fixed difficulty scaling for trench and narrow passage

legged_gym/utils/terrain.py
@@ -119,7 +119,8 @@ class Terrain:
         stone_distance = self.cfg.horizontal_difficulty_scale * (0.05 + 0.35 * difficulty)
         gap_size = self.cfg.horizontal_difficulty_scale * difficulty
         pit_depth = self.cfg.steps_height_scale * 0.5 * difficulty
-        trench_width = self.cfg.horizontal_difficulty_scale  * 0.46 * (1 - (0.04 * difficulty))
+        trench_width = self.cfg.horizontal_difficulty_scale  * 0.62 * (1 - (0.1 * difficulty))
+        passage_width = self.cfg.horizontal_difficulty_scale  * 0.75 * (1 - (0.15 * difficulty))
         if choice < self.proportions[0]:
             if choice < self.proportions[0]/ 2:
                 slope *= -1
@@ -144,7 +145,7 @@ class Terrain:
         elif choice < self.proportions[7]:
             radial_trench_terrain(terrain, wall_height=0.8, trench_width=trench_width, num_trenches=8, inner_untouched_diameter_percent=0.16, outer_untouched_diameter_percent=0.46)
         else:
-            radial_trench_terrain_with_gaps(terrain, gap_depth=1., trench_width=trench_width, num_trenches=8, inner_untouched_diameter_percent=0.16, outer_untouched_diameter_percent=0.46)
+            radial_trench_terrain_with_gaps(terrain, gap_depth=1., passage_width=passage_width, num_trenches=8, inner_untouched_diameter_percent=0.16, outer_untouched_diameter_percent=0.46)
             #pit_terrain(terrain, depth=pit_depth, platform_size=4.)
         
         return terrain
@@ -241,7 +242,7 @@ def radial_trench_terrain(terrain, wall_height, trench_width, num_trenches, inne
             if not trench_points[x1, y1]:
                 terrain.height_field_raw[x1, y1] += wall_height
 
-def radial_trench_terrain_with_gaps(terrain, gap_depth, trench_width, num_trenches, inner_untouched_diameter_percent, outer_untouched_diameter_percent):
+def radial_trench_terrain_with_gaps(terrain, gap_depth, passage_width, num_trenches, inner_untouched_diameter_percent, outer_untouched_diameter_percent):
     """
     Modify the terrain height field to add radial gaps, leaving the trenches at the original level and creating
     gaps or cliffs between them.
@@ -254,7 +255,7 @@ def radial_trench_terrain_with_gaps(terrain, gap_depth, trench_width, num_trench
     outer_untouched_diameter_percent: Outer diameter percentage that remains untouched.
     """
     # Adjusting gap dimensions based on terrain scale
-    trench_width = int(trench_width / terrain.horizontal_scale)
+    passage_width = int(passage_width / terrain.horizontal_scale)
 
     # Determining the center of the terrain
     center_x = terrain.length // 2
@@ -282,7 +283,7 @@ def radial_trench_terrain_with_gaps(terrain, gap_depth, trench_width, num_trench
                     angle_diff = min(abs(angle - angle_to_point), 
                                      abs(angle - angle_to_point - 2 * np.pi), 
                                      abs(angle - angle_to_point + 2 * np.pi))
-                    if angle_diff < trench_width / distance_to_center:
+                    if angle_diff < passage_width / distance_to_center:
                         trench_points[x1, y1] = True  # Mark as trench
 
     # Raising non-trench areas